9ce686efe061a70005c123ba767b09138740888b
Sateesh Monica <sateeshmonica@Sateeshs-MBP.lan>
2020-10-06 23:27:54 -0500

    Refactoring

--- src/com/addu/Main.java
+++
@@ -1,8 +0,0 @@
-package com.addu;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Addu is a great boy!");
-    }
-}
---
+++ src/com/automation/Main.java
@@ -0,0 +1,8 @@
+package com.automation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Automation is Awesome!");
+    }
+}
